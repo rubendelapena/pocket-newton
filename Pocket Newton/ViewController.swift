23aa1e8871d1d062680a2519ab7cba34be9a0ae1
Ruben de la Pena <rubendelapena@rubens-macbook-air.local>
2018-02-18 14:25:41 -0600

    Initial commit

---
+++ Pocket Newton/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pocket Newton
+//
+//  Created by Ruben de la Pena on 2/18/18.
+//  Copyright © 2018 Rubén de la Peña. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Pods/Target Support Files/Pods-Pocket Newton/Pods-Pocket Newton-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Pocket_Newton : NSObject
+@end
+@implementation PodsDummy_Pods_Pocket_Newton
+@end
